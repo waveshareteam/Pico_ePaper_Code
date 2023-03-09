510d896c1b459e11b16bb95a8d216f02f892b9e1
mbartlett21 <mjmouse9999@gmail.com>
2023-03-09 15:27:14 +1000

    Add comments to existing code for 2.9 inch display
    Also re-layout the waveform arrays to be clearer

--- c/lib/e-Paper/EPD_2in9_V2.c
+++ c/lib/e-Paper/EPD_2in9_V2.c
@@ -33,49 +33,77 @@
 
 UBYTE _WF_PARTIAL_2IN9[159] =
 {
-0x0,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x80,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x40,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0A,0x0,0x0,0x0,0x0,0x0,0x2,  
-0x1,0x0,0x0,0x0,0x0,0x0,0x0,
-0x1,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x22,0x22,0x22,0x22,0x22,0x22,0x0,0x0,0x0,
-0x22,0x17,0x41,0xB0,0x32,0x36,
+0,          0b01000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // LUT 0 (black to black)
+0b10000000, 0b10000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // LUT 1 (black to white)
+0b01000000, 0b01000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // LUT 2 (white to black)
+0,          0b10000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // LUT 3 (white to white)
+0,          0,          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // LUT 4
+//TP[A]
+//  TP[B]
+//      SR[AB]
+//          TB[C]
+//              TB[D]
+//                  SR[CD]
+//                      RP
+10, 0,  0,  0,  0,  0,  2, // Group 0
+1,  0,  0,  0,  0,  0,  0, // Group 1
+1,  0,  0,  0,  0,  0,  0, // Group 2
+0,  0,  0,  0,  0,  0,  0, // Group 3
+0,  0,  0,  0,  0,  0,  0, // Group 4
+0,  0,  0,  0,  0,  0,  0, // Group 5
+0,  0,  0,  0,  0,  0,  0, // Group 6
+0,  0,  0,  0,  0,  0,  0, // Group 7
+0,  0,  0,  0,  0,  0,  0, // Group 8
+0,  0,  0,  0,  0,  0,  0, // Group 9
+0,  0,  0,  0,  0,  0,  0, // Group 11
+0,  0,  0,  0,  0,  0,  0, // Group 12
+
+0x22,0x22,0x22,0x22,0x22,0x22, // Framerates (FR[0] to FR[11])
+0, 0, 0, // Gate scan selection (XON)
+0x22, // EOPT = Normal
+0x17, // VGH  = 20 V
+0x41, // VSH1 = 15 V
+0xB0, // VSH2 = 5.8 V
+0x32, // VSL  = -15 V
+0x36, // VCOM = -1.3 to -1.4 (not shown on datasheet)
 };
 
 UBYTE WS_20_30[159] =
-{											
-0x80,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x40,	0x0,	0x0,	0x0,
-0x10,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x20,	0x0,	0x0,	0x0,
-0x80,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x40,	0x0,	0x0,	0x0,
-0x10,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x20,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x14,	0x8,	0x0,	0x0,	0x0,	0x0,	0x1,					
-0xA,	0xA,	0x0,	0xA,	0xA,	0x0,	0x1,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x14,	0x8,	0x0,	0x1,	0x0,	0x0,	0x1,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x1,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,					
-0x44,	0x44,	0x44,	0x44,	0x44,	0x44,	0x0,	0x0,	0x0,			
-0x22,	0x17,	0x41,	0x0,	0x32,	0x36
-};	
+{
+//   0           1      2  3  4  5  6  7       8      9 10 11
+0b10000000, 0b01100110, 0, 0, 0, 0, 0, 0, 0b01000000, 0, 0, 0, // LUT 0 (black to black)
+0b00010000, 0b01100110, 0, 0, 0, 0, 0, 0, 0b00100000, 0, 0, 0, // LUT 1 (black to white)
+0b10000000, 0b01100110, 0, 0, 0, 0, 0, 0, 0b01000000, 0, 0, 0, // LUT 2 (white to black)
+0b00010000, 0b01100110, 0, 0, 0, 0, 0, 0, 0b00100000, 0, 0, 0, // LUT 3 (white to white)
+0,          0,          0, 0, 0, 0, 0, 0, 0,          0, 0, 0, // LUT 4
+//TP[A]
+//  TP[B]
+//      SR[AB]
+//          TB[C]
+//              TB[D]
+//                  SR[CD]
+//                      RP
+20, 8,  0,  0,  0,  0,  1, // Group 0
+10, 10, 0,  10, 10, 0,  1, // Group 1
+0,  0,  0,  0,  0,  0,  0, // Group 2
+0,  0,  0,  0,  0,  0,  0, // Group 3
+0,  0,  0,  0,  0,  0,  0, // Group 4
+0,  0,  0,  0,  0,  0,  0, // Group 5
+0,  0,  0,  0,  0,  0,  0, // Group 6
+0,  0,  0,  0,  0,  0,  0, // Group 7
+20, 8,  0,  1,  0,  0,  1, // Group 8
+0,  0,  0,  0,  0,  0,  1, // Group 9
+0,  0,  0,  0,  0,  0,  0, // Group 11
+0,  0,  0,  0,  0,  0,  0, // Group 12
+0x44, 0x44, 0x44, 0x44, 0x44, 0x44, // Framerates (FR[0] to FR[11])
+0, 0, 0, // Gate scan selection (XON)
+0x22, // EOPT = Normal
+0x17, // VGH  = 20V
+0x41, // VSH1 = 15 V
+0,    // VSH2 = Unknown
+0x32, // VSL  = -15 V
+0x36, // VCOM = -1.3 to -1.4 (not shown on datasheet)
+};
 
 /******************************************************************************
 function :	Software reset
@@ -126,7 +154,7 @@ void EPD_2IN9_V2_ReadBusy(void)
     Debug("e-Paper busy\r\n");
 	while(1)
 	{	 //=1 BUSY
-		if(DEV_Digital_Read(EPD_BUSY_PIN)==0) 
+		if(DEV_Digital_Read(EPD_BUSY_PIN)==0)
 			break;
 		DEV_Delay_ms(50);
 	}
@@ -137,9 +165,9 @@ void EPD_2IN9_V2_ReadBusy(void)
 static void EPD_2IN9_V2_LUT(UBYTE *lut)
 {
 	UBYTE count;
-	EPD_2IN9_V2_SendCommand(0x32);
-	for(count=0; count<153; count++) 
-		EPD_2IN9_V2_SendData(lut[count]); 
+	EPD_2IN9_V2_SendCommand(0x32); // Write LUT Register
+	for(count=0; count<153; count++)
+		EPD_2IN9_V2_SendData(lut[count]);
 	EPD_2IN9_V2_ReadBusy();
 }
 
@@ -165,17 +193,17 @@ parameter:
 ******************************************************************************/
 static void EPD_2IN9_V2_TurnOnDisplay(void)
 {
-	EPD_2IN9_V2_SendCommand(0x22); //Display Update Control
-	EPD_2IN9_V2_SendData(0xC7);
-	EPD_2IN9_V2_SendCommand(0x20); //Activate Display Update Sequence
+	EPD_2IN9_V2_SendCommand(0x22); // Display Update Control 2
+	EPD_2IN9_V2_SendData(0xC7); // Display mode 1
+	EPD_2IN9_V2_SendCommand(0x20); // Master Activation
 	EPD_2IN9_V2_ReadBusy();
 }
 
 static void EPD_2IN9_V2_TurnOnDisplay_Partial(void)
 {
-	EPD_2IN9_V2_SendCommand(0x22); //Display Update Control
-	EPD_2IN9_V2_SendData(0x0F);   
-	EPD_2IN9_V2_SendCommand(0x20); //Activate Display Update Sequence
+	EPD_2IN9_V2_SendCommand(0x22); // Display Update Control 2
+	EPD_2IN9_V2_SendData(0x0F); // Display mode 2
+	EPD_2IN9_V2_SendCommand(0x20); // Master Activation
 	EPD_2IN9_V2_ReadBusy();
 }
 
@@ -185,11 +213,11 @@ parameter:
 ******************************************************************************/
 static void EPD_2IN9_V2_SetWindows(UWORD Xstart, UWORD Ystart, UWORD Xend, UWORD Yend)
 {
-    EPD_2IN9_V2_SendCommand(0x44); // SET_RAM_X_ADDRESS_START_END_POSITION
+    EPD_2IN9_V2_SendCommand(0x44); // Set RAM X Address Start/End
     EPD_2IN9_V2_SendData((Xstart>>3) & 0xFF);
     EPD_2IN9_V2_SendData((Xend>>3) & 0xFF);
-	
-    EPD_2IN9_V2_SendCommand(0x45); // SET_RAM_Y_ADDRESS_START_END_POSITION
+
+    EPD_2IN9_V2_SendCommand(0x45); // Set RAM Y Address Start/End
     EPD_2IN9_V2_SendData(Ystart & 0xFF);
     EPD_2IN9_V2_SendData((Ystart >> 8) & 0xFF);
     EPD_2IN9_V2_SendData(Yend & 0xFF);
@@ -202,10 +230,10 @@ parameter:
 ******************************************************************************/
 static void EPD_2IN9_V2_SetCursor(UWORD Xstart, UWORD Ystart)
 {
-    EPD_2IN9_V2_SendCommand(0x4E); // SET_RAM_X_ADDRESS_COUNTER
+    EPD_2IN9_V2_SendCommand(0x4E); // Set RAM X Address Counter
     EPD_2IN9_V2_SendData(Xstart & 0xFF);
 
-    EPD_2IN9_V2_SendCommand(0x4F); // SET_RAM_Y_ADDRESS_COUNTER
+    EPD_2IN9_V2_SendCommand(0x4F); // Set RAM Y Address Counter
     EPD_2IN9_V2_SendData(Ystart & 0xFF);
     EPD_2IN9_V2_SendData((Ystart >> 8) & 0xFF);
 }
@@ -219,26 +247,26 @@ void EPD_2IN9_V2_Init(void)
 	EPD_2IN9_V2_Reset();
 	DEV_Delay_ms(100);
 
-	EPD_2IN9_V2_ReadBusy();   
-	EPD_2IN9_V2_SendCommand(0x12); // soft reset
 	EPD_2IN9_V2_ReadBusy();
-	
-	EPD_2IN9_V2_SendCommand(0x01); //Driver output control      
+	EPD_2IN9_V2_SendCommand(0x12); // SW Reset
+	EPD_2IN9_V2_ReadBusy();
+
+	EPD_2IN9_V2_SendCommand(0x01); // Driver Output Control
 	EPD_2IN9_V2_SendData(0x27);
 	EPD_2IN9_V2_SendData(0x01);
 	EPD_2IN9_V2_SendData(0x00);
-	
-	EPD_2IN9_V2_SendCommand(0x11); //data entry mode       
+
+	EPD_2IN9_V2_SendCommand(0x11); // Data Entry Mode Setting
 	EPD_2IN9_V2_SendData(0x03);
-	
+
 	EPD_2IN9_V2_SetWindows(0, 0, EPD_2IN9_V2_WIDTH-1, EPD_2IN9_V2_HEIGHT-1);
-	
-	EPD_2IN9_V2_SendCommand(0x21); //  Display update control
+
+	EPD_2IN9_V2_SendCommand(0x21); // Display Update Control 1
 	EPD_2IN9_V2_SendData(0x00);
-	EPD_2IN9_V2_SendData(0x80);	
-	
+	EPD_2IN9_V2_SendData(0x80);
+
 	EPD_2IN9_V2_SetCursor(0, 0);
-	EPD_2IN9_V2_ReadBusy();	
+	EPD_2IN9_V2_ReadBusy();
 
 	EPD_2IN9_V2_LUT_by_host(WS_20_30);
 }
@@ -250,7 +278,7 @@ parameter:
 void EPD_2IN9_V2_Clear(void)
 {
 	UWORD i;
-	EPD_2IN9_V2_SendCommand(0x24);   //write RAM for black(0)/white (1)
+	EPD_2IN9_V2_SendCommand(0x24); // Write RAM (B/W)
 	for(i=0;i<4736;i++)
 	{
 		EPD_2IN9_V2_SendData(0xff);
@@ -265,29 +293,29 @@ parameter:
 void EPD_2IN9_V2_Display(UBYTE *Image)
 {
 	UWORD i;	
-	EPD_2IN9_V2_SendCommand(0x24);   //write RAM for black(0)/white (1)
+	EPD_2IN9_V2_SendCommand(0x24); // Write RAM (B/W)
 	for(i=0;i<4736;i++)
 	{
 		EPD_2IN9_V2_SendData(Image[i]);
 	}
-	EPD_2IN9_V2_TurnOnDisplay();	
+	EPD_2IN9_V2_TurnOnDisplay();
 }
 
 void EPD_2IN9_V2_Display_Base(UBYTE *Image)
 {
-	UWORD i;   
+	UWORD i;
 
-	EPD_2IN9_V2_SendCommand(0x24);   //Write Black and White image to RAM
+	EPD_2IN9_V2_SendCommand(0x24); // Write RAM (B/W)
 	for(i=0;i<4736;i++)
-	{               
+	{
 		EPD_2IN9_V2_SendData(Image[i]);
 	}
-	EPD_2IN9_V2_SendCommand(0x26);   //Write Black and White image to RAM
+	EPD_2IN9_V2_SendCommand(0x26); // Write RAM (RED)
 	for(i=0;i<4736;i++)
-	{               
+	{
 		EPD_2IN9_V2_SendData(Image[i]);
 	}
-	EPD_2IN9_V2_TurnOnDisplay();	
+	EPD_2IN9_V2_TurnOnDisplay();
 }
 
 void EPD_2IN9_V2_Display_Partial(UBYTE *Image)
@@ -301,34 +329,34 @@ void EPD_2IN9_V2_Display_Partial(UBYTE *Image)
     DEV_Delay_ms(2);
 
 	EPD_2IN9_V2_LUT(_WF_PARTIAL_2IN9);
-	EPD_2IN9_V2_SendCommand(0x37); 
-	EPD_2IN9_V2_SendData(0x00);  
-	EPD_2IN9_V2_SendData(0x00);  
-	EPD_2IN9_V2_SendData(0x00);  
-	EPD_2IN9_V2_SendData(0x00); 
-	EPD_2IN9_V2_SendData(0x00);  
-	EPD_2IN9_V2_SendData(0x40);  
-	EPD_2IN9_V2_SendData(0x00);  
-	EPD_2IN9_V2_SendData(0x00);   
-	EPD_2IN9_V2_SendData(0x00);  
+	EPD_2IN9_V2_SendCommand(0x37); // Write Register for Display Option
+	EPD_2IN9_V2_SendData(0x00); // Default VCOM OTP
+	EPD_2IN9_V2_SendData(0x00); // WS[7:0]
+	EPD_2IN9_V2_SendData(0x00); // WS[15:8]
+	EPD_2IN9_V2_SendData(0x00); // WS[23:16] Display modes are set to 1
+	EPD_2IN9_V2_SendData(0x00); // WS[31:24]
+	EPD_2IN9_V2_SendData(0x40); // Ping Pong enable for display mode 2
+	EPD_2IN9_V2_SendData(0x00); // Module id / waveform version is 0
+	EPD_2IN9_V2_SendData(0x00);
+	EPD_2IN9_V2_SendData(0x00);
 	EPD_2IN9_V2_SendData(0x00);
 
-	EPD_2IN9_V2_SendCommand(0x3C); //BorderWaveform
-	EPD_2IN9_V2_SendData(0x80);	
+	EPD_2IN9_V2_SendCommand(0x3C); // Border Waveform Control
+	EPD_2IN9_V2_SendData(0x80);
 
-	EPD_2IN9_V2_SendCommand(0x22); 
-	EPD_2IN9_V2_SendData(0xC0);   
-	EPD_2IN9_V2_SendCommand(0x20); 
-	EPD_2IN9_V2_ReadBusy();  
-	
+	EPD_2IN9_V2_SendCommand(0x22); // Display Update Control 2
+	EPD_2IN9_V2_SendData(0xC0);
+	EPD_2IN9_V2_SendCommand(0x20); // Master Activation
+	EPD_2IN9_V2_ReadBusy();
+
 	EPD_2IN9_V2_SetWindows(0, 0, EPD_2IN9_V2_WIDTH-1, EPD_2IN9_V2_HEIGHT-1);
 	EPD_2IN9_V2_SetCursor(0, 0);
 
-	EPD_2IN9_V2_SendCommand(0x24);   //Write Black and White image to RAM
+	EPD_2IN9_V2_SendCommand(0x24); // Write RAM (B/W)
 	for(i=0;i<4736;i++)
 	{
 		EPD_2IN9_V2_SendData(Image[i]);
-	} 
+	}
 	EPD_2IN9_V2_TurnOnDisplay_Partial();
 }
 
@@ -338,7 +366,7 @@ parameter:
 ******************************************************************************/
 void EPD_2IN9_V2_Sleep(void)
 {
-	EPD_2IN9_V2_SendCommand(0x10); //enter deep sleep
-	EPD_2IN9_V2_SendData(0x01); 
+	EPD_2IN9_V2_SendCommand(0x10); // Deep Sleep Mode
+	EPD_2IN9_V2_SendData(0x01);
 	DEV_Delay_ms(100);
 }

18cb4ab716268bea01f069f30750016f68c3db73
shhds <1873145421@qq.com>
2023-10-20 17:51:22 +0800

    Add 2.13B V4 landscape display program

--- python/Pico_ePaper-2.13-B_V4.py
+++ python/Pico_ePaper-2.13-B_V4.py
@@ -40,7 +40,7 @@ DC_PIN          = 8
 CS_PIN          = 9
 BUSY_PIN        = 13
 
-class EPD_2in13_B_V4:
+class EPD_2in13_B_V4_Portrait:
     def __init__(self):
         self.reset_pin = Pin(RST_PIN, Pin.OUT)
         
@@ -196,9 +196,168 @@ class EPD_2in13_B_V4:
         self.delay_ms(2000)
         self.module_exit()
         
+class EPD_2in13_B_V4_Landscape:
+    def __init__(self):
+        self.reset_pin = Pin(RST_PIN, Pin.OUT)
+        
+        self.busy_pin = Pin(BUSY_PIN, Pin.IN, Pin.PULL_UP)
+        self.cs_pin = Pin(CS_PIN, Pin.OUT)
+        if EPD_WIDTH % 8 == 0:
+            self.width = EPD_WIDTH
+        else :
+            self.width = (EPD_WIDTH // 8) * 8 + 8
+        self.height = EPD_HEIGHT
+        
+        self.spi = SPI(1)
+        self.spi.init(baudrate=4000_000)
+        self.dc_pin = Pin(DC_PIN, Pin.OUT)
+        
+        
+        self.buffer_balck = bytearray(self.height * self.width // 8)
+        self.buffer_red = bytearray(self.height * self.width // 8)
+        self.imageblack = framebuf.FrameBuffer(self.buffer_balck, self.height, self.width, framebuf.MONO_VLSB)
+        self.imagered = framebuf.FrameBuffer(self.buffer_red, self.height, self.width, framebuf.MONO_VLSB)
+        self.init()
+
+    def digital_write(self, pin, value):
+        pin.value(value)
+
+    def digital_read(self, pin):
+        return pin.value()
+
+    def delay_ms(self, delaytime):
+        utime.sleep(delaytime / 1000.0)
+
+    def spi_writebyte(self, data):
+        self.spi.write(bytearray(data))
+
+    def module_exit(self):
+        self.digital_write(self.reset_pin, 0)
+
+    # Hardware reset
+    def reset(self):
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(2)
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)
+
+
+    def send_command(self, command):
+        self.digital_write(self.dc_pin, 0)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([command])
+        self.digital_write(self.cs_pin, 1)
+
+    def send_data(self, data):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([data])
+        self.digital_write(self.cs_pin, 1)
+        
+    def send_data1(self, buf):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi.write(bytearray(buf))
+        self.digital_write(self.cs_pin, 1)
+        
+    def ReadBusy(self):
+        print('busy')
+        while(self.digital_read(self.busy_pin) == 1): 
+            self.delay_ms(10) 
+        print('busy release')
+        self.delay_ms(20)
+        
+    def TurnOnDisplay(self):
+        self.send_command(0x20)  # Activate Display Update Sequence
+        self.ReadBusy()
+
+    def SetWindows(self, Xstart, Ystart, Xend, Yend):
+        self.send_command(0x44) # SET_RAM_X_ADDRESS_START_END_POSITION
+        self.send_data((Xstart>>3) & 0xFF)
+        self.send_data((Xend>>3) & 0xFF)
+
+        self.send_command(0x45) # SET_RAM_Y_ADDRESS_START_END_POSITION
+        self.send_data(Ystart & 0xFF)
+        self.send_data((Ystart >> 8) & 0xFF)
+        self.send_data(Yend & 0xFF)
+        self.send_data((Yend >> 8) & 0xFF)
+        
+    def SetCursor(self, Xstart, Ystart):
+        self.send_command(0x4E) # SET_RAM_X_ADDRESS_COUNTER
+        self.send_data(Xstart & 0xFF)
+
+        self.send_command(0x4F) # SET_RAM_Y_ADDRESS_COUNTER
+        self.send_data(Ystart & 0xFF)
+        self.send_data((Ystart >> 8) & 0xFF)
+    
+
+    def init(self):
+        print('init')
+        self.reset()
+        
+        self.ReadBusy()   
+        self.send_command(0x12)  #SWRESET
+        self.ReadBusy()   
+
+        self.send_command(0x01) #Driver output control      
+        self.send_data(0xf9)
+        self.send_data(0x00)
+        self.send_data(0x00)
+
+        self.send_command(0x11) #data entry mode       
+        self.send_data(0x07)
+
+        self.SetWindows(0, 0, self.width-1, self.height-1)
+        self.SetCursor(0, 0)
+
+        self.send_command(0x3C) #BorderWaveform
+        self.send_data(0x05)
+
+        self.send_command(0x18) #Read built-in temperature sensor
+        self.send_data(0x80)
+
+        self.send_command(0x21) #  Display update control
+        self.send_data(0x80)
+        self.send_data(0x80)
+
+        self.ReadBusy()
+        
+        return 0       
+        
+    def display(self):
+        self.send_command(0x24)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(self.buffer_balck[i + j * self.height])
+        
+        self.send_command(0x26)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(self.buffer_red[i + j * self.height])
+
+        self.TurnOnDisplay()
+
+    
+    def Clear(self, colorblack, colorred):
+        self.send_command(0x24)
+        self.send_data1([colorblack] * self.height * int(self.width / 8))
+        
+        self.send_command(0x26)
+        self.send_data1([colorred] * self.height * int(self.width / 8))
+                                
+        self.TurnOnDisplay()
+
+    def sleep(self):
+        self.send_command(0x10) 
+        self.send_data(0x01)
+        
+        self.delay_ms(2000)
+        self.module_exit()
         
 if __name__=='__main__':
-    epd = EPD_2in13_B_V4()
+    epd = EPD_2in13_B_V4_Portrait()
     epd.Clear(0xff, 0xff)
     
     epd.imageblack.fill(0xff)
@@ -221,6 +380,32 @@ if __name__=='__main__':
     
     epd.imageblack.rect(10, 150, 40, 40, 0x00)
     epd.imagered.fill_rect(60, 150, 40, 40, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+
+    epd = EPD_2in13_B_V4_Landscape()
+    epd.Clear(0xff, 0xff)
+
+    epd.imageblack.fill(0xff)
+    epd.imagered.fill(0xff)
+    epd.imageblack.text("Waveshare", 0, 10, 0x00)
+    epd.imagered.text("ePaper-2.13B", 0, 20, 0x00)
+    epd.imageblack.text("Raspberry Pico", 0, 30, 0x00)
+    epd.imagered.text("Hello World", 0, 40, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+
+    epd.imagered.vline(5, 55, 60, 0x00)
+    epd.imagered.vline(100, 55, 60, 0x00)
+    epd.imageblack.hline(5, 55, 95, 0x00)
+    epd.imageblack.hline(5, 115, 95, 0x00)
+    epd.imagered.line(5, 55, 100, 115, 0x00)
+    epd.imageblack.line(100, 55, 5, 115, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+    
+    epd.imageblack.rect(130, 10, 40, 80, 0x00)
+    epd.imagered.fill_rect(190, 10, 40, 80, 0x00)
     epd.display()
     epd.delay_ms(2000)
         
